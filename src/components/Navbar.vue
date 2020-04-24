3b9af75b9949988a2f6754a0d82d8cab31f1ad5e
Hrishikesh Paul <hrpaul@iu.edu>
2020-04-23 22:16:13 -0400

    improved nav collapse button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
           <Logo :nightMode="nightMode" />
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span style="color: gray; font-size: 23px;"><i class="fas fa-bars"></i></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
