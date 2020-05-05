c2b85465946a6a6f6e21f378ce864a02ab6b2514
Hrishikesh Paul <hrpaul@iu.edu>
2020-05-05 17:04:35 -0400

    added tiny logo animation

--- src/components/helpers/Logo.vue
+++ src/components/helpers/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="logo-div">
         <span class="logo-title-name" :class="{'pgray': !nightMode, 'text-light': nightMode}">{{name}}</span>
         <span class="logo-title-dot pblue">{{dot}}</span>
     </div>
@@ -25,10 +25,16 @@ export default {
 </script>
 
 <style scoped>
+.logo-div:hover .logo-title-name{
+    letter-spacing: 0px;
+    transition: 0.5s all;
+}
+
 .logo-title-name {
     font-size: 35px;
     font-weight: 500;
-    letter-spacing: 1px;
+    letter-spacing: 2px;
+    transition: 0.5s all;
 }
 
 .logo-title-dot {
