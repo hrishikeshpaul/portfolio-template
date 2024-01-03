00a71c19dd3539c4e4055a27edb8e0f9a227b4a9
Mohamad Al Mdfaa <mohamad.almdfaa@gmail.com>
2024-01-03 16:42:01 +0000

    refactor: update info

--- info.js
+++ info.js
@@ -167,20 +167,20 @@ let info = {
   ],
   portfolio: [
     {
-      name: "Vision-based Multi-rotor Aerial Vehicles Tracking for a Moving Object (Publication)",
+      name: "3D-SiamMask (Publication)",
       pictures: [
         {
-          img: require("./src/assets/portfolio/msr/one.png")
+          img: require("./src/assets/portfolio/3dsiammask/3d-siammask-000.jpeg")
         },
         {
-          img: require("./src/assets/portfolio/msr/two.png")
+          img: require("./src/assets/portfolio/3dsiammask/3d-siammask-001.png")
         },
         {
-          img: require("./src/assets/portfolio/msr/three.png")
+          img: require("./src/assets/portfolio/3dsiammask/3d-siammask-002.png")
         },
         {
-          img: require("./src/assets/portfolio/msr/four.png")
-        }
+          img: require("./src/assets/portfolio/3dsiammask/3d-siammask-003.png")
+        },
       ],
       technologies: ["Visual Object Tracking", "Deep Learning", "Camera Calibration", "Epipolar Geometry",
       "ROS", "Python", "C++", "PyTorch", "OpenCV", "Docker", "Linux","NVidia Jetson Hardware", "TensorRT"],
@@ -196,20 +196,10 @@ let info = {
       name: "RETA (Restaurant ETA)",
       pictures: [
         {
-          img: require("./src/assets/portfolio/noq/logo.png")
+          img: require("./src/assets/portfolio/reta/reta-beeorder.png")
         },
-        {
-          img: require("./src/assets/portfolio/noq/one.png")
-        },
-        {
-          img: require("./src/assets/portfolio/noq/two.png")
-        },
-        {
-          img: require("./src/assets/portfolio/noq/three.png")
-        },
-        {
-          img: require("./src/assets/portfolio/noq/four.png")
-        }
+        
+
       ],
       technologies: ["Clustering",
       "Python",
@@ -226,249 +216,124 @@ let info = {
         "Geo-based unsupervised learning approach to estimate delivery time across the platform's outlets: As an AI Engineer at BeeOrder, I worked on the Restaurant ETA (RETA) service, where I developed a geo-based unsupervised learning approach to estimate delivery time across the platform's outlets. This project required the use of various technologies, including Clustering, Python, Redis, MariaDB, OSRM, Flask, and others. The goal of the project was to improve the accuracy and efficiency of the delivery time estimation process, which would ultimately benefit both the restaurants and their customers. In order to achieve this goal, I implemented advanced machine learning techniques and optimized the system for real-time performance. The successful completion of this project helped to drive the growth and success of the company."
     },
     {
-      name: "POST",
+      name: "ANA",
       pictures: [
         {
-          img: require("./src/assets/portfolio/post/one.png")
+          img: require("./src/assets/portfolio/ana/ana-spacenus.png")
         },
         {
-          img: require("./src/assets/portfolio/post/two.png")
+          img: require("./src/assets/portfolio/ana/ana.jpg")
         }
       ],
       technologies: [
-        "Markov Models",
-        "Python",
-        "Artificial Intelligence",
-        "Vue",
-        "Heroku"
+        "Machin Learning", "Deep Learning", "Computer Vision","Research", "Python", "AWS"
       ],
-      category: "Algorithm",
-      github: "https://github.com/hrishikeshpaul/pos-tagger",
-      date: "Oct, 2019 - Nov, 2019",
-      visit: "https://post-client.herokuapp.com/",
+      category: "Production &  Research",
+      date: "June, 2019 - Aug, 2020",
+      visit: "https://www.ana.farm/",
       description:
-        "Post is a simple algorithm that was developed to tagging a word corresponding to its part of speech. The algorithm makes of a probabilisitic approach along with some randomness, together which forms the basis of an algorithm called Gibbs Sampling."
+        "Agricultural Nutrient Assistant (ANA) and Plant Nutrient Detection (PND): I was part of the team responsible for the development and maintenance of the Agricultural Nutrient Assistant (ANA) project, focusing on refining and managing the mathematical and machine learning models integral to its functionality. My key contribution involved crafting the Plant Nutrient Detection (PND) tool, utilizing cutting-edge image analysis and machine learning techniques. This tool accurately identifies and measures plant nutrient levels from images captured via a mobile phone camera. To enable the seamless deployment of ANA APIs (ana.farm), I leveraged Flask, Docker, and an AWS stack encompassing EC2, Fargate, Lambda, and S3. This implementation led to remarkable cost efficiency, reducing expenses by over 90%. My tech toolkit encompassed Python, Tensorflow, OpenCV, Numpy, Flask, AWS, and Docker."
     },
     {
-      name: "Suicide Analyzer",
+      name: "Neural Indoor Localization",
       pictures: [
         {
-          img: require("./src/assets/portfolio/suicide/suicide-analyzer.png")
+          img: require("./src/assets/portfolio/navigine-inddor-localization/nvgn-002.png")
         },
         {
-          img: require("./src/assets/portfolio/suicide/suicide2-original.png")
+          img: require("./src/assets/portfolio/navigine-inddor-localization/nvgn.jpg")
+        },
+        {
+          img: require("./src/assets/portfolio/navigine-inddor-localization/gyro-integration.png")
         }
       ],
-      technologies: ["Node", "Angular", "Express", "Leaflet", "Pandas"],
-      category: "Website",
-      date: "Oct, 2018 - Nov, 2018",
-      github: "https://github.com/hrishikeshpaul/whodata",
-      visit: "https://skylarktiral.herokuapp.com/",
+      technologies: ["Image Retrieval", "Keypoint Extraction", "Gyroscope Integration", "Visual SLAM", "Python", "Open3D", "Docker"],
+      category: "Research",
+      date: "Dec, 2022 - April, 2022",
+      visit: "https://sites.skoltech.ru/mobilerobotics/",
       description:
-        "This is a web application, uses a map to demonstrate the number suicides committed around the globe, according to a dataset provided by the WHO. The application consists of a Leaflet map that colors every nation, based on the number of suicides that have been committed in that country, for that year. It provides us with the capability to select the data as well as the continent."
+        "Contributed to Neural Visual Localization by utilizing techniques such as Visual SLAM, Keypoint Extraction, Image Retrieval, and IMU Integration."
     },
     {
-      name: "Sentiment Analyzer",
+      name: "Field-Boundary",
       pictures: [
         {
-          img: require("./src/assets/portfolio/smit/one.png")
+          img: require("./src/assets/portfolio/field-boundary/Field-boundary-logo.png")
         },
         {
-          img: require("./src/assets/portfolio/smit/two.png")
+          img: require("./src/assets/portfolio/field-boundary/Fields-all-shapes-and-forms.png")
         },
         {
-          img: require("./src/assets/portfolio/smit/three.png")
+          img: require("./src/assets/portfolio/field-boundary/Field-Boundary-App-01.png")
         },
         {
-          img: require("./src/assets/portfolio/smit/four.png")
+          img: require("./src/assets/portfolio/field-boundary/Detetected-field-boundaries.png")
         }
       ],
-      technologies: ["LSTM", "Keras", "Angular", "Tweepy", "Heroku", "Python"],
-      category: "Alogrithm",
-      date: "Feb, 2018 - Apr  , 2018",
-      github: "https://github.com/hrishikeshpaul/sih",
-      visit: "https://angel.co/projects/1009777-sentiment-analysis",
+      technologies: ["Python", "OpenCV", "Albumentation", "Numpy", "Scipy", "Scikit-image", "GDAL", "rasterio", "AWS Lambda"],
+      category: "Production",
+      date: "April, 2019 - May , 2019",
+      visit: "https://www.field-boundary.com/",
       description:
-        "Developing this for the final round of Smart India Hackathon 2018, the web application is a platform for showcasing the sentiment of the tweets of the people in the Telangana region in India on policy satisfaction, services and problem definitions. The minimal UI design consists of different bins, that have their respective tweets, all scraped from twitter. There is also a simple graphical representation denoting the sentiment of the tweets."
+        "Improved the Field Boundary API by using image processing and data augmentation techniques, making the Field Boundary model more accurate."
     },
     {
-      name: "Clumpr",
+      name: "Crowd-Copter",
       pictures: [
         {
-          img: require("./src/assets/portfolio/clumpr/logo.png")
+          img: require("./src/assets/portfolio/crowd-copter/Crowd Management.png")
         },
         {
-          img: require("./src/assets/portfolio/clumpr/one.png")
+          img: require("./src/assets/portfolio/crowd-copter/Flowchart.jpeg")
         },
         {
-          img: require("./src/assets/portfolio/clumpr/two.png")
+          img: require("./src/assets/portfolio/crowd-copter/overview.png")
         }
       ],
       technologies: ["Node", "Angular", "Express", "MySQL", "Facebook API"],
       category: "Web App",
-      github: "https://github.com/hrishikeshpaul/clumpr",
-      date: "Sep, 2017 - Nov, 2017",
-      visit: "https://github.com/hrishikeshpaul/clumpr",
+      github: "https://github.com/mhd-medfa/Crowd-Copter",
+      date: "Oct, 2016 - May, 2017",
+      visit: "https://github.com/mhd-medfa/Crowd-Copter",
       description:
-        'Clumpr is an online social event planning assistant that - using information about similar events happening in similar locations around the same time, friends who frequently attend the same events, and mutual friends - helps users to collaborate, combine, and attend events together, effectively "clumping" people and activities in a way that makes intuitive sense.'
+        'Implemented unsupervised learning and computer vision approach for crowd movement analysis.'
     },
-    {
-      name: "Nutricare",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/nc/one.png")
-        },
-        {
-          img: require("./src/assets/portfolio/nc/two.png")
-        },
-        {
-          img: require("./src/assets/portfolio/nc/three.png")
-        },
-        {
-          img: require("./src/assets/portfolio/nc/four.png")
-        },
-        {
-          img: require("./src/assets/portfolio/nc/five.png")
-        },
-        {
-          img: require("./src/assets/portfolio/nc/six.png")
-        }
-      ],
-      technologies: ["Node", "Angular", "Express", "MongDB", "Medical"],
-      category: "Website",
-      github: "https://github.com/hrishikeshpaul/clumpr",
-      date: "May, 2017 - Aug, 2017",
-      visit: "https://angel.co/projects/576300-nutricare",
-      description:
-        "Nutricare is a basic health website for users(patients) to input nutrient intake data and researchers to analyse the data with the help of graphs and tables. A researcher can conduct studies on N different subjects (patient) for a particular problem statement. Typically, a researcher will enter food item consumed by subjects over a period of time (Ex - 24hrs, or 1 week, or 1 month etc)."
-    }
   ],
-  portfolio_design: [
-    {
-      name: "Guituna",
-      title: "Guituna - Mockup Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/coursera1/MoodBoard.png"),
-          title: "MoodBoard"
-        },
-        {
-          img: require("./src/assets/designs/coursera1/Mockups 1.png"),
-          title: "Mockups 1"
-        },
-        {
-          img: require("./src/assets/designs/coursera1/Mockups 2.png"),
-          title: "Mockups 2"
-        },
-        {
-          img: require("./src/assets/designs/coursera1/App Elements.png"),
-          title: "App Elements"
-        }
-      ],
-      technologies: ["XD", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "May, 2020 - Jun, 2020",
-      visit: "",
-      description:
-        "Guituna is a simple, lightweight and intuitive guitar tuner, that provides different modes of guitars based on your usage. You can either select a specific string to tune, or free tune your guitar based on the frequency shown on the meter. Guituna also provides a handful of different tunings to help explore various tuning paradigms. There is also an array of settings that can be configured to your liking based on the devices you're using. <br/><br/>The interface aims to highlight the use of a minimal design and providing just enough controls to make for a great tuner, while exposing various possibilities of guitarists to explore."
-    },
-    {
-      name: "Pantree",
-      title: "Pantree - Mockup Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/pantree/MoodBoard.png"),
-          title: "Moodboard"
-        },
-        {
-          img: require("./src/assets/designs/pantree/1.png"),
-          title: "Mockups 1"
-        },
-        {
-          img: require("./src/assets/designs/pantree/2.png"),
-          title: "Mockups 2"
-        }
-      ],
-      technologies: ["XD", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "May, 2020 - July, 2020",
-      visit: "",
-      description:
-        "Pantree is a recipiece finding and recommending application. It is often the case that you have run to your closest grocery store just to pick up one item that is missing in your kitchen. Pantree keeps a track of all your ingredients, and suggests recipes based on your meal preferences and past recipes. <br/><br/> The goal of the interface is to keep it similar to various recipe/food apps to reduce the learning required by the user, but also provide a smooth interface for users to seemlessly browse through recipes and ingredients."
-    },
-    {
-      name: "Bunder",
-      title: "Bunder - MVP Proposal",
-      pictures: [
-        {
-          img: require("./src/assets/designs/bunder/Moodboard.png"),
-          title: "Moodboard"
-        },
-        {
-          img: require("./src/assets/designs/bunder/Mockup 1.png"),
-          title: "Mockup 1"
-        },
-        {
-          img: require("./src/assets/designs/bunder/Mockup 2.png"),
-          title: "Mockup 2"
-        },
-        {
-          img: require("./src/assets/designs/bunder/Mockup 3.png"),
-          title: "Mockup 3"
-        },
-        {
-          img: require("./src/assets/designs/bunder/Market Research 1.png"),
-          title: "Market Research 1"
-        },
-        {
-          img: require("./src/assets/designs/bunder/Market Research 2.png"),
-          title: "Market Research 2"
-        }
-      ],
-      technologies: ["XD", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "May, 2020 - July, 2020",
-      visit: "",
-      description:
-        "Bunder is an intuitive web platform for micro-communities to share sensitive data with privacy within the community and allow engagement amongst them. Our aim is two-fold - Co-existing and Collaboration. The former is executed by providing a platform for communities that are geographically positioned in the same proximity (Housing societies, Dorms, Apartment Complexes) to have and share data (exact apartment addresses, phone number, apartment housing rules, individuals tested positive for COVID-19) in an exclusive platform. The latter is to give these people in the community the ability to carry out a task you are physically or remotely unable to do; with dignity and ease."
-    }
-  ],
+  
   recommendations: [
     {
       title:
-        "In his internship, Hrishikesh has demonstrated excellent learning ability, and with a dedicated, task oriented approach, he was able to complete his assignment ahead of time.",
-      author: "Ushanas Shastri",
-      position: "CTO",
-      company: "Viteos Capital Market Services",
-      location: "Mumbai"
+        "Mohamad is a great AI Engineer, although we had a very challenging project at BeeOrder, he still was able to overcome all obstacles and took the project to live environment. Later he anticipated in creating new AI functionality that optimized our work and saved us a lot of resources and time . Muhammad always make sure that the work he applied have a high positive impact and ROI, this is how he prioritizes his tasks that why I always loved working with him directly. A fast learner and a good coach for other Junior Engineers.",
+      author: "Abdel Malek Al-Mouzayen",
+      position: "Co-Founder & Vice President",
+      company: "BeeOrder",
     },
     {
       title:
-        "I feel his budding leadership abilities will become even more effective in a diverse and challenging environment.",
-      author: "Anil Dukkipatty",
-      position: "CTO",
-      company: "Elemential Labs",
-      location: "Mumbai"
+      `I can strongly recommend Mohamad – I would absolutely take him as a strong and reliable
+      early career scientist in Machine Learning or NLP. He goes above and beyond in research,
+      engagement, and project management. He is a highly-motivated individual with excellent
+      communication skills, and strongly technically capable.
+      Mohamad’s written English needs no editing and his conversation skills in English are also
+      clearly of professional grade (I am a native speaker). He integrates in groups well and
+      positively. Mr. al Mdfaa is ambitious and motivated, and already assembling creative
+      approaches to research problems, and accompanying them with meaningful and rigorous
+      analyses.
+      I highly recommend Mohamad – he is a strong researcher and will benefit any group.`,
+      author: "Dr. Leon R.A Strømberg-Derczynski",
+      position: "Associate Professor",
+      company: "IT University of Copenhagen",
+      location: "Copenhagen, Denmark"
     },
     {
       title:
-        "He has gained great knowledge and experience of SDE, and has learned ot develop the application keeping in mind the client's perpective and also making it creative.",
-      author: "Chintan Shah",
-      position: "Director",
-      company: "Hridayam Soft Solution",
-      location: "Mumbai"
+      'Mohamad and I worked for the same brand "L-One". Mohammad worked as a Machine Learning Engineer. He demonstrated very good research skills, analytical skills, and solid experience in ML and Python programming. In addition to this, Mohamad is very decent and committed. I highly recommend working with him.',
+      author: "Feras Tanan",
+      position: "Head of Software Development",
+      company: "L-One Systems GmbH",
+      location: "Darmstadt, Germany"
     },
-    {
-      title:
-        "During the course of his employment we have found him to be a self-started who was motivated, duty bound and a highly commited team player.",
-      author: "Mrinal Pai",
-      position: "Co-Founder & Director",
-      company: "Skylark Drones",
-      location: "Bangalore"
-    }
+
   ]
 };
 
